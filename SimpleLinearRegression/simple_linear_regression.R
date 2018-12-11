0f243f77b2e93ab794a7cf1f1889534f98dc3210
GorgiNeta <gorgineta@gmail.com>
2018-12-11 11:45:36 +0100

    026SLR_in_R_S3

--- SimpleLinearRegression/simple_linear_regression.R
+++ SimpleLinearRegression/simple_linear_regression.R
@@ -15,3 +15,9 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 # training_set[, 2:3] = scale(training_set[, 2:3])
 # test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting the Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience, training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
